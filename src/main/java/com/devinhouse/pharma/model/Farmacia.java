4a86067af7780a2e2b8ffadf091c7ddcb302f69f
Denison Kolling <denisonkolling@gmail.com>
2023-11-27 17:22:09 -0300

    adiciona entidade farmacia

---
+++ src/main/java/com/devinhouse/pharma/model/Farmacia.java
@@ -0,0 +1,35 @@
+package com.devinhouse.pharma.model;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "FARMACIAS")
+public class Farmacia {
+
+    @Id
+    private Long cnpj;
+
+    private String razaoSocial;
+
+    private String nomeFantasia;
+
+    private String email;
+
+    private String telefone;
+
+    private String celular;
+
+    @Embedded
+    private Endereco endereco;
+
+}
