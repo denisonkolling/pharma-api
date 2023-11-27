601cde8d617c7dd9b1be9c0211d7e40ac624c804
Denison Kolling <denisonkolling@gmail.com>
2023-11-27 18:26:09 -0300

    adiciona anotacao repository

--- src/main/java/com/devinhouse/pharma/repository/EstoqueRepository.java
+++ src/main/java/com/devinhouse/pharma/repository/EstoqueRepository.java
@@ -2,6 +2,8 @@ package com.devinhouse.pharma.repository;
 
 import com.devinhouse.pharma.model.Estoque;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EstoqueRepository extends JpaRepository<Estoque, Integer> {
 }
--- src/main/java/com/devinhouse/pharma/repository/MedicamentoRepository.java
+++ src/main/java/com/devinhouse/pharma/repository/MedicamentoRepository.java
@@ -2,6 +2,8 @@ package com.devinhouse.pharma.repository;
 
 import com.devinhouse.pharma.model.Medicamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MedicamentoRepository extends JpaRepository<Medicamento, Integer> {
 }
