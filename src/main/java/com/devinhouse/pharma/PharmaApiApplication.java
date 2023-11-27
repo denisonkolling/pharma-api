97082dfce3b07adc8f83580d96a11983061b4ccf
Denison Kolling <denisonkolling@gmail.com>
2023-11-27 16:17:27 -0300

    primeiro commit

---
+++ src/main/java/com/devinhouse/pharma/PharmaApiApplication.java
@@ -0,0 +1,13 @@
+package com.devinhouse.pharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devinhouse/pharma/PharmaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.pharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
