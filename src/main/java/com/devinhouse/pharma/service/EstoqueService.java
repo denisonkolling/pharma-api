a2eb91ac220bb318705627172c37e0060f61d784
Denison Kolling <denisonkolling@gmail.com>
2023-11-28 09:41:40 -0300

    adiciona metodos service estoque

--- src/main/java/com/devinhouse/pharma/service/EstoqueService.java
+++ src/main/java/com/devinhouse/pharma/service/EstoqueService.java
@@ -1,10 +1,19 @@
 package com.devinhouse.pharma.service;
 
+import com.devinhouse.pharma.model.Estoque;
+
+import java.util.List;
+
+
 public interface EstoqueService {
+
+    // TODO RF08 - Aquisição de Medicamentos para Estoque de farmácia
+
+    Estoque cadastrarEstoque(Estoque estoque);
 
     // TODO RF07 - Consulta de Estoque de Farmácia
 
-    // TODO RF08 - Aquisição de Medicamentos para Estoque de farmácia
+    List<Estoque> consultarEstoquePorCnpj(Long cnpj);
 
     // TODO RF09 - Venda de Medicamentos com atualização do Estoque de farmácia
 
