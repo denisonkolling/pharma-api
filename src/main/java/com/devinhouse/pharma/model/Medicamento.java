631ff6875ee6a9b8e3c6e32ad037ed81f76ddc81
Denison Kolling <denisonkolling@gmail.com>
2023-11-29 09:36:55 -0300

    corrige id entidade medicamento

--- src/main/java/com/devinhouse/pharma/model/Medicamento.java
+++ src/main/java/com/devinhouse/pharma/model/Medicamento.java
@@ -14,7 +14,6 @@ import lombok.NoArgsConstructor;
 public class Medicamento {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer nroRegistro;
 
     private String nome;
