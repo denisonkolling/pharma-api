3dee1524f5967f556e54ab495d37884ba356af68
Denison Kolling <denisonkolling@gmail.com>
2023-11-29 09:34:34 -0300

    adciciona endpoint listagem medicamentos e medicamento response dto

--- src/main/java/com/devinhouse/pharma/controller/MedicamentoController.java
+++ src/main/java/com/devinhouse/pharma/controller/MedicamentoController.java
@@ -1,14 +1,17 @@
 package com.devinhouse.pharma.controller;
 
+import com.devinhouse.pharma.dto.FarmaciaResponse;
+import com.devinhouse.pharma.dto.MedicamentoResponse;
+import com.devinhouse.pharma.model.Farmacia;
 import com.devinhouse.pharma.model.Medicamento;
 import com.devinhouse.pharma.service.MedicamentoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicamentos")
@@ -17,9 +20,22 @@ public class MedicamentoController {
     @Autowired
     MedicamentoService medicamentoService;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @PostMapping
     public ResponseEntity<?> cadastrarMedicamento(@RequestBody Medicamento medicamento) {
         return new ResponseEntity<>(medicamentoService.cadastrarMedicamento(medicamento), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MedicamentoResponse>> listarTodosMedicamentos() {
+        List<Medicamento> medicamentos = medicamentoService.listarTodosMedicamentos();
+        var response = medicamentos
+                .stream()
+                .map(medicamento -> mapper.map(medicamento, MedicamentoResponse.class))
+                .toList();
+        return ResponseEntity.ok(response);
     }
 
 }
